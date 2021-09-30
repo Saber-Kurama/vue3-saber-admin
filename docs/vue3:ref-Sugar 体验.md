ba7530728c8f3f1541afa4c4b2e465d7035ece6d
guochao <463561248@qq.com>
2021-09-30 16:53:21 +0800

    :asd feat:asd

---
+++ docs/vue3:ref-Sugar 体验.md
@@ -0,0 +1,3 @@
+## vue3
+
+[vue3:ref-Sugar 体验](https://github.com/GeekQiaQia/Blob/blob/master/refSugar.md)
---
+++ src/components/Application/src/AppLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ count }}</div>
+  <button @click="add">click me</button>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const count = ref(1);
+  function add() {
+    count.value++;
+  }
+</script>
